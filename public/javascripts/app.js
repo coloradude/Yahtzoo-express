db3a05a845a25dd393b5d31ec4f35cef21e824c8
Stephen Florian <steezeven@gmail.com>
2015-09-28 13:43:34 -0600

    testing

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -249,6 +249,7 @@ $(document).ready(function(){
     if(e.which == 13){
       $('.lets-play').click();
     }
+  preventDoubleTap();
   });
 
   $('.big-button').on('click', function(e){
@@ -526,7 +527,24 @@ function scoreSubmit(){
   });
 }
 
+function preventDoubleTap(){
+  (function($) {
+    $.fn.nodoubletapzoom = function() {
+        $(this).bind('touchstart', function preventZoom(e) {
+          var t2 = e.timeStamp
+            , t1 = $(this).data('lastTouch') || t2
+            , dt = t2 - t1
+            , fingers = e.originalEvent.touches.length;
+          $(this).data('lastTouch', t2);
+          if (!dt || dt > 500 || fingers > 1) return; // not double-tap
 
+          e.preventDefault(); // double tap - prevent the zoom
+          // also synthesize click events we just swallowed up
+          $(this).trigger('click').trigger('click');
+        });
+    };
+  })(jQuery);
+}
 
 
 
